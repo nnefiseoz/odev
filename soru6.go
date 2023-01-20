4f62a6eaa8e1adc2da04464bcc29290b4d61e379
Nefise Öztürk <119896761+nnefiseoz@users.noreply.github.com>
2023-01-20 15:56:20 +0300

    Create soru6.go

---
+++ soru6.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	arr := []int{5, 5, 5, 10, 5}
+	toplam := 0
+	for i := 0; i < len(arr); i++ {
+		toplam += arr[i]
+	}
+	ortalama := (float64(toplam)) / 5
+	fmt.Println("Toplam = ", toplam, "Ortalma = ", ortalama)
+}
